14c9460e4da00ea735585fa5c41d97873d41565c
hberker <hosni.berker@gmail.com>
2019-10-04 14:40:36 -0400

    adds changes to defense

--- my-algo/algo_strategy.py
+++ my-algo/algo_strategy.py
@@ -28,8 +28,8 @@ class AlgoStrategy(gamelib.AlgoCore):
         EMP = config["unitInformation"][4]["shorthand"]
         SCRAMBLER = config["unitInformation"][5]["shorthand"]
         self.scored_on_locations = []
-        self.destructorLocations = [[1,12],[26,12],[7,10],[20,10],[11,7],[16,7]]
-        self.filterLocations = [[7,11],[1,13],[26,13],[20,11],[6,11],[9,10],[0,13],[2,12],[3,11],[24,11],[25,12],[27,13]]
+        self.destructorLocations = [[1,12],[26,12],[7,10],[20,10],[11,7],[16,7],[19,10],[8,10],[25,11],[2,11],[15,6],[12,6]]
+        self.filterLocations = [[7,11],[1,13],[26,13],[20,11],[6,11],[9,10],[0,13],[2,12],[3,11],[25,12],[27,13],[19,11],[8,11],[21,11],[22,11],[23,11],[5,11],[4,11],[18,10],[10,9],[17,9],[13,11],[14,11]]
     def on_turn(self, turn_state):
         """
         This function is called every turn with the game state wrapper as
@@ -48,13 +48,16 @@ class AlgoStrategy(gamelib.AlgoCore):
         self.empAttack(game_state)
         self.pingAttacks(game_state)
         game_state.submit_turn()
+
     def placeScram(self, gamestate):
         gamestate.attempt_spawn(SCRAMBLER, [14,1])
+
     def placeEnc(self, gamestate):
         gamestate.attempt_spawn(ENCRYPTOR, [[13,2],[14,2],[13,3],[14,3]])
         for spot in [[13,2],[14,2],[13,3],[14,3]]:
             if gamestate.contains_stationary_unit(spot):
                 self.encryptorCount += 1
+
     def placeDefense(self, gamestate):
         gamestate.attempt_spawn(DESTRUCTOR,self.destructorLocations)
         return
@@ -72,6 +75,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         if gamestate.number_affordable(PING) >= 5 and self.encryptorCount >= 4 and self.sentEMP :
             gamestate.attempt_spawn(PING, [13,0],5)
         return
+
     def on_action_frame(self, turn_string):
         """
         This is the action frame of the game. This function could be called 
